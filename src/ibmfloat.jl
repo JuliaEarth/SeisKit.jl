679a13775480fee1211051e092339def46ac88b5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-16 10:32:42 -0300

    Fixes to ibmfloat.jl

--- src/ibmfloat.jl
+++ src/ibmfloat.jl
@@ -19,27 +19,41 @@ If S = 1, the number is negative; if S = 0, the number is positive.
 
 The value represented is given by
 
-    (-1)^S × (FFFFFFFF|FFFFFFFF|FFFFFFFF) × 16^(EEEEEEE - 64)
+    (-1)^S × 0.(FFFFFFFF|FFFFFFFF|FFFFFFFF) × 16^(EEEEEEE - 64)
 
-If the fraction is zero, the value is zero regardless of the exponent or sign.
+If the fraction is zero, the value is zero regardless of the exponent.
 """
 primitive type IBMFloat32 <: AbstractFloat 32 end
 
-function convert(::Type{Float32}, x::IBMFloat32)
-  # extract 4 bytes from IBMFloat32
-  b1, b2, b3, b4 = reinterpret(NTuple{4,UInt8}, x)
-
-  # compute fraction from last 3 bytes
-  F = reinterpret(UInt32, (zero(UInt8), b2, b3, b4))
+function Base.convert(::Type{Float64}, x::IBMFloat32)
+  b = reinterpret(UInt32, x)
+  s = b & 0x80000000
+  e = b & 0x7f000000
+  f = b & 0x00ffffff
 
   # if fraction is zero, value is zero
-  iszero(F) && return zero(Float32)
+  iszero(f) && return zero(Float64)
 
-  # compute sign from first bit
-  S = b1 >> 7 # shift first bit all the way to the right
+  # shift sign bit to the rightmost position
+  sieee = Int64(s >> 31)
 
-  # compute exponent from remaining 7 bits
-  E = b1 & 0b01111111 # knock out first bit in byte
+  # shift exponent bits to the right (3 bytes = 24 bits)
+  # and multiply by 4 to convert from base 16 to base 2
+  # hence, the resulting shift is 24 - 2 = 22 bits
+  eieee = Int64(e >> 22)
 
-  Float32((-1)^S * F * 16^(E - 64))
+  # normalize fraction until the leading digit is equal to 1,
+  # and adjust the exponent accordingly (opposite shift)
+  d = f & 0x00f00000
+  while iszero(d)
+    f <<= 4
+    eieee -= 4
+    d = f & 0x00f00000
+  end
+  fieee = UInt64(f) << 32 # pad with zeros to the right
+
+  # what is the correct way to combine the parts into Float64?
+  sieee, eieee, fieee
 end
+
+Base.read(io::IO, ::Type{IBMFloat32}) = reinterpret(IBMFloat32, read(io, UInt32))
