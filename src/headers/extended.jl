9174b58f1c50cb14cbca1eedf490bc607b2edfb3
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-17 09:21:15 -0300

    Implement extendedheaders

--- src/headers/extended.jl
+++ src/headers/extended.jl
@@ -11,4 +11,23 @@ extendedheaders(fname::AbstractString) = open(extendedheaders, fname)
     extendedheaders(io::IO) -> Vector{ExtendedHeader}
 """
 function extendedheaders(io::IO)
+  # seek start of extended headers
+  seek(io, 3600)
+
+  # decode extended headers
+  headers = String[]
+  for _ in 1:nextendedheaders(io)
+    # read 3200 bytes
+    bytes = read(io, 3200)
+
+    # identify encoding from first byte
+    # '(' is 0x28 in ASCII and 0x4D in EBCDIC
+    encoding = first(bytes) == 0x28 ? "ASCII" : "EBCDIC-CP-US"
+
+    # decode bytes with identified encoding
+    decode(bytes, encoding)
+  end
+
+  # return extended headers as strings
+  headers
 end 
