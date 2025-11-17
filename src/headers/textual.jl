88ace8d753b4a8d238ad895d550a3dc680cc6c0e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-17 08:54:03 -0300

    Recognize ASCII vs. EBCDIC in textual header

--- src/headers/textual.jl
+++ src/headers/textual.jl
@@ -15,7 +15,9 @@ textualheader(fname::AbstractString) = open(textualheader, fname)
 Read the SEG-Y textual header from the IO stream `io`.
 """
 function textualheader(io::IO)
-  # decode first 3200 bytes as EBCDIC
+  # identify encoding from first byte
+  # ('C' is 0x43 in ASCII and 0xC3 in EBCDIC)
+  # and decode 3200 bytes accordingly
   bytes = read(seekstart(io), 3200)
-  decode(bytes, "EBCDIC-CP-US")
+  first(bytes) == 0x43 ? String(bytes) : decode(bytes, "EBCDIC-CP-US")
 end
