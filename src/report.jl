3ec335dff71c66dac594d91d316f3e9894736137
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-20 12:23:10 -0300

    Add more reports

--- src/report.jl
+++ src/report.jl
@@ -49,10 +49,47 @@ function printheaders(th, bh, eh, trh)
   pretty_table((; field, minimum, maximum);
     title="SEG-Y Trace Header Summary (non-zero fields only)",
     fit_table_in_display_vertically=false,
-    new_line_at_end=false,
     alignment=:l,
   )
 end
 
 function reportissues(th, bh, eh, trh)
+  issues = String[]
+  if any(iszero, trh.SAMPLES_IN_TRACE)
+    push!(issues, """
+      - Detected trace headers with SAMPLES_IN_TRACE=0.
+        The value SAMPLES_PER_TRACE from the binary header
+        should be copied to all trace headers.
+      """
+    )
+  end
+  if isone(bh.FIXED_LENGTH_TRACE_FLAG)
+    if !allequal(trh.SAMPLES_IN_TRACE)
+      push!(issues, """
+        - Detected FIXED_LENGTH_TRACE_FLAG=1 in binary header
+          with varying SAMPLES_IN_TRACE in trace headers.
+          The flag should be set to 0 to indicate variable
+          length traces.
+        """
+      )
+    elseif !all(==(bh.SAMPLES_PER_TRACE), trh.SAMPLES_IN_TRACE)
+      push!(issues, """
+        - Detected FIXED_LENGTH_TRACE_FLAG=1 in binary header
+          with SAMPLES_IN_TRACE in trace headers different
+          from SAMPLES_PER_TRACE in binary header. The value
+          SAMPLES_IN_TRACE from trace headers should be copied
+          to the binary header.
+        """
+      )
+    end
+  end
+
+  println()
+
+  if isempty(issues)
+    println("No SEG-Y issues detected.")
+  else
+    println("SEG-Y issues report:\n")
+    println(join(issues, "\n"))
+  end
 end
