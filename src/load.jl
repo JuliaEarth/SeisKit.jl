3375ef3de319b9ea3ae7769a18a3fd4c3bab50d9
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-21 18:08:03 -0300

    First load implementation

--- src/load.jl
+++ src/load.jl
@@ -16,4 +16,58 @@ Load SEG-Y data from the IO stream `io`.
 """
 function load(io::IO)
   th, bh, eh, trh = headers(io)
+  traces(io, bh, eh, trh)
+end
+
+function traces(io, bh, eh, trh)
+  # determine number of traces
+  ntraces = length(trh)
+
+  # determine number of samples in each trace
+  nsamples = Int.(replace(trh.SAMPLES_IN_TRACE, 0 => bh.SAMPLES_PER_TRACE))
+
+  # determine number of dimensions (2D or 3D seismic)
+  ninline = length(unique(trh.INLINE_NUMBER))
+  ncross = length(unique(trh.CROSSLINE_NUMBER))
+  ndims = ninline > 1 && ncross > 1 ? 3 : 2
+
+  # load traces with optimized method
+  if ndims == 2
+    if allequal(nsamples)
+      fixedtraces2D(io, bh, eh, trh, first(nsamples), ntraces)
+    else
+      throw(ErrorException("Variable-length 2D seismic data loading not yet implemented"))
+    end
+  else
+    throw(ErrorException("3D seismic data loading not yet implemented"))
+  end
+end
+
+function fixedtraces2D(io, bh, eh, trh, m, n)
+  # swap bytes if necessary
+  swapbytes = isbigendian(io) ? ntoh : ltoh
+
+  # determine final number type for samples
+  T = numbertype(io)
+  F = T <: IBMFloat32 ? Float64 : T
+
+  # pre-allocate data array with final number type
+  data = Array{F}(undef, m, n)
+
+  # seek start of trace data
+  seek(io, TEXTUAL_HEADER_SIZE + BINARY_HEADER_SIZE + nextendedheaders(io) * EXTENDED_HEADER_SIZE)
+
+  # load all traces
+  for j in 1:n
+    # skip trace header
+    skip(io, TRACE_HEADER_SIZE)
+
+    # read trace samples
+    for i in 1:m
+      v = swapbytes(read(io, T))
+      @inbounds data[i, j] = convert(F, v)
+    end
+  end
+
+  data
 end
