777c18df93a4bfee0ce5d4ba67ddf3ff5e75fe7a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-12-04 10:35:37 -0300

    Refactor tests

--- test/segy/dataset.jl
+++ test/segy/dataset.jl
@@ -1,9 +1,9 @@
 @testset "Dataset" begin
   d = Segy.load(joinpath(datadir, "stacked2Drev1.sgy"))
-  x = Segy.rawcoords(d)
-  @test length(x) == length(d.traces)
-  @test unit(first(first(x))) == u"m"
+  xy = Segy.rawcoords(d)
+  @test length(xy) == length(d.traces)
+  @test unit(first(first(xy))) == u"m"
   @test Segy.datum(d) === WGS84Latest
   @test_throws ErrorException Segy.crs(d)
-  @test Segy.coords(d) == [Cartesian{WGS84Latest}(x, y) for (x, y) in x]
+  @test Segy.coords(d) == [Cartesian{WGS84Latest}(x, y) for (x, y) in xy]
 end
--- test/segy/headers.jl
+++ test/segy/headers.jl
@@ -6,8 +6,8 @@ end
 
 @testset "TraceHeader" begin
   hs = Segy.traceheaders(joinpath(datadir, "stacked2Drev1.sgy"))
-  xs = Segy.rawcoords.(hs)
-  @test length(xs) == length(hs)
-  @test all(x -> 466000u"m" < x[1] < 473600u"m", xs)
-  @test all(x -> 7194309u"m" < x[2] < 7197755u"m", xs)
+  xy = Segy.rawcoords.(hs)
+  @test length(xy) == length(hs)
+  @test all(x -> 466000u"m" < x[1] < 473600u"m", xy)
+  @test all(x -> 7194309u"m" < x[2] < 7197755u"m", xy)
 end
